54a0f75215bb58783aa8002890f2d8cc8d88172f
ytxfinance <contact@ytx.finance>
2020-11-07 21:49:01 +0100

    Updated token contract with transfer fee

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,13 +1,13 @@
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades')
 const DistributeEarnings = artifacts.require('DistributeEarnings')
 const YTX = artifacts.require('YTX')
-const YTXV2 = artifacts.require('YTXV2')
+const YTXV3 = artifacts.require('YTXV3')
 
 module.exports = async function(deployer) {
   const existing = await YTX.deployed()
   // 1. Initial deployment
   // await deployProxy(YTX, [], { deployer, initializer: 'initialize' });
   // 2. Deploy upgraded contract
-  await upgradeProxy(existing.address, YTXV2, { deployer })
+  await upgradeProxy(existing.address, YTXV3, { deployer })
   // await deployProxy(DistributeEarnings);
 }
